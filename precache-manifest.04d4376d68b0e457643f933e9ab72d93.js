e0f8701879a195954c0cd0d1dd49180d430fef8f
Safa Tinaztepe <safa.tinaztepe@gmail.com>
2019-06-04 12:06:37 -0700

    Updates

---
+++ precache-manifest.04d4376d68b0e457643f933e9ab72d93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33fb9d0806be1db9b21003d1f0315ca4",
+    "url": "/nback-react/index.html"
+  },
+  {
+    "revision": "1bd5c70d4bf7bad8a430",
+    "url": "/nback-react/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "24e01ba2576f51c84293",
+    "url": "/nback-react/static/css/main.b949787d.chunk.css"
+  },
+  {
+    "revision": "1bd5c70d4bf7bad8a430",
+    "url": "/nback-react/static/js/2.ed0c4e93.chunk.js"
+  },
+  {
+    "revision": "24e01ba2576f51c84293",
+    "url": "/nback-react/static/js/main.fabd7c26.chunk.js"
+  },
+  {
+    "revision": "14295e2fb9ad482a3b9c",
+    "url": "/nback-react/static/js/runtime~main.29723e00.js"
+  }
+]);
